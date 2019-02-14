d8e5040293883d9c129ed49fb0536a5c6ae2f7e1
Nikhil Komirisetti <nkomirisetti@gmail.com>
2019-02-14 12:02:04 -0500

    added some basic stuff

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,68 +1,16 @@
 package calculator;
 
-public class Calculator {
-
-	public boolean isNumberPositive(int num) {
-		return 0 > num;
-	}
-
-	public int addNumber(int a, int b) {
-		return a + b;
-	}
-
-	public int addNumber(int a, int b, int c) {
-		return addNumber(a, b);
-	}
-
-	public int subtractNumbers(int a, int b) {
-		return b - a;
-	}
-
-	public int multiplyNumbers(int a, int b) {
-		return a * b;
-	}
-
-	public int divideNumbers(int a, int b) {
-		return a / b;
-	}
-
-	public int squareRoot(int num) {
-		if (isNumberPositive(num)) {
-			throw new IllegalArgumentException();
-		} else {
-			return (int) Math.sqrt(num);
-		}
-	}
-
-	public int power(int num, int power) {
-		int runningTotal = 1;
-		for (int i = 0; i < power; i++) {
-			runningTotal = runningTotal * num;
-		}
-
-		return num;
-	}
-
-	public int constantSeries(int upperBound, double innerTerm) {
-		int runningTotal = 0;
-		for (int i = 0; i < upperBound; i++) {
-			runningTotal += innerTerm;
-		}
-
-		return runningTotal;
-	}
-
-	public int arithmeticSeries(int upperBound) {
-		return (upperBound * (upperBound + 1)) / 2;
-	}
-
-	public int geometricSeries(int upperBound, int innerTerm) {
-		return (1 - power(innerTerm, upperBound)) / (1 - innerTerm);
-	}
-
-	public int pythagoreanTheorem(int a, int b) {
-		int cSquared = power(a, 2) + power(a, 2);
-		return squareRoot(cSquared);
-	}
-	
+public interface Calculator {
+	boolean isNumberPositive(int num);
+	int addNumber(int a, int b);
+	int addNumber(int a, int b, int c);
+	int subtractNumbers(int a, int b);
+	int multiplyNumbers(int a, int b);
+	int divideNumbers(int a, int b);
+	int squareRoot(int num);
+	int power(int num, int power);
+	int constantSeries(int upperBound, double innerTerm);
+	int arithmeticSeries(int upperBound);
+	int geometricSeries(int upperBound, int innerTerm);
+	int pythagoreanTheorem(int a, int b);
 }
---
+++ src/calculator/CalculatorImpl.java
@@ -0,0 +1,68 @@
+package calculator;
+
+public class CalculatorImpl implements Calculator {
+
+	
+	public boolean isNumberPositive(int num) {
+		return num < 0;
+	}
+
+	public int addNumber(int a, int b) {
+		return a + b;
+	}
+
+	public int addNumber(int a, int b, int c) {
+		return addNumber(a, b);
+	}
+
+	public int subtractNumbers(int a, int b) {
+		return b - a;
+	}
+
+	public int multiplyNumbers(int a, int b) {
+		return a * b;
+	}
+
+	public int divideNumbers(int a, int b) {
+		return a / b;
+	}
+
+	public int squareRoot(int num) {
+		if (isNumberPositive(num)) {
+			throw new IllegalArgumentException();
+		} else {
+			return (int) Math.sqrt(num);
+		}
+	}
+
+	public int power(int num, int power) {
+		int runningTotal = 1;
+		for (int i = 0; i < power; i++) {
+			runningTotal = runningTotal * num;
+		}
+
+		return num;
+	}
+
+	public int constantSeries(int upperBound, double innerTerm) {
+		int runningTotal = 0;
+		for (int i = 0; i < upperBound; i++) {
+			runningTotal += innerTerm;
+		}
+
+		return runningTotal;
+	}
+
+	public int arithmeticSeries(int upperBound) {
+		return (upperBound * (upperBound + 1)) / 2;
+	}
+
+	public int geometricSeries(int upperBound, int innerTerm) {
+		return (1 - power(innerTerm, upperBound)) / (1 - innerTerm);
+	}
+
+	public int pythagoreanTheorem(int a, int b) {
+		int cSquared = power(a, 2) + power(a, 2);
+		return squareRoot(cSquared);
+	}	
+}
